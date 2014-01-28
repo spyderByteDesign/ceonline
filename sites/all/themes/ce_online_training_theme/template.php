ed7e570c004c0d2ef5e37bcb067ec676fb26037b
Ryan Wagner <wagner@palantir.net>
2014-01-28 00:43:22 -0500

    Adds a new sub-theme for New Fire to style. Creates template file for course nodes. Creates advanced pre-processing function to generate link from course to view with filters for content related to view. Updates display modes for courses/videos.

---
+++ sites/all/themes/ce_online_training_theme/template.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @file
+ * This file is empty by default because the base theme chain (Alpha & Omega) provides
+ * all the basic functionality. However, in case you wish to customize the output that Drupal
+ * generates through Alpha & Omega this file is a good place to do so.
+ * 
+ * Alpha comes with a neat solution for keeping this file as clean as possible while the code
+ * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
+ * for more information on this topic.
+ */
+
+function ce_online_training_theme_preprocess_html(&$variables) {
+  // Add conditional stylesheets for IE
+  // drupal_add_css(path_to_theme() . '/css/commerce-kickstart-theme-ie-lte-8.css', array('group' => CSS_THEME, 'weight' => 23, 'browsers' => array('IE' => 'lte IE 8', '!IE' => FALSE), 'preprocess' => FALSE));
+  // drupal_add_css(path_to_theme() . '/css/commerce-kickstart-theme-ie-lte-7.css', array('group' => CSS_THEME, 'weight' => 24, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'preprocess' => FALSE));
+
+  // Add external libraries.
+  drupal_add_library('commerce_kickstart_theme', 'selectnav');
+}
+
+/**
+ * Implements hook_library().
+ */
+function ce_online_training_theme_library() {
+  $libraries['selectnav'] = array(
+    'title' => 'Selectnav',
+    'version' => '',
+    'js' => array(
+      libraries_get_path('selectnav.js') . '/selectnav.min.js' => array(),
+    ),
+  );
+  return $libraries;
+}
+
+/**
+ * Override the submitted variable.
+ */
+function ce_online_training_theme_preprocess_node(&$variables) {
+  $variables['submitted'] = $variables['date'] . ' - ' . $variables['name'];
+  if ($variables['type'] == 'blog_post') {
+    $variables['submitted'] = t('By') . ' ' . $variables['name'] . ', ' . $variables['date'];
+  }
+
+  if ($variables['type'] == 'course') {
+    $term = taxonomy_get_term_by_name($variables['title']);
+    if (!empty($term)) {
+      $variables['course_links'] = array();
+      foreach ($term as $value) {
+        $variables['course_links'][] = l($value->name . ' Course Videos', 'course-videos', array('query' => array('tid' => $value->tid)));
+      }
+    }
+  }
+}
