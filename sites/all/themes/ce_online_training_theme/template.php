c0bf856281db987e5ef41f9ad044a451edea7b21
Ryan Wagner <wagner@palantir.net>
2014-02-06 22:50:24 -0500

    Reduces redundant queries in preprocess node by storing course taxonomy term in variables.

--- sites/all/themes/ce_online_training_theme/template.php
+++ sites/all/themes/ce_online_training_theme/template.php
@@ -5,7 +5,7 @@
  * This file is empty by default because the base theme chain (Alpha & Omega) provides
  * all the basic functionality. However, in case you wish to customize the output that Drupal
  * generates through Alpha & Omega this file is a good place to do so.
- * 
+ *
  * Alpha comes with a neat solution for keeping this file as clean as possible while the code
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
@@ -44,7 +44,9 @@ function ce_online_training_theme_preprocess_node(&$variables) {
   }
 
   if ($variables['type'] == 'course') {
-    $term = taxonomy_get_term_by_name($variables['title']);
+    if (empty($variables['course_term'])) {
+      $variables['course_term'] = taxonomy_get_term_by_name($variables['title']);
+    }
     if (!empty($term)) {
       $variables['course_links'] = array();
       foreach ($term as $value) {
