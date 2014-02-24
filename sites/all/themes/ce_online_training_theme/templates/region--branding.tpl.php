ea581bf8fce47adddacceacd9d913b0d9821ce16
Chris Zietlow <zietlow@palantir.net>
2014-02-23 23:27:36 -0500

    Updated styles to make menu look more like mockups

--- sites/all/themes/ce_online_training_theme/templates/region--branding.tpl.php
+++ sites/all/themes/ce_online_training_theme/templates/region--branding.tpl.php
@@ -1,28 +1,19 @@
 <div<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
-    <?php if ($linked_logo_img || $site_name || $site_slogan): ?>
+    <?php if ($linked_logo_img): ?>
     <div class="branding-data">
       <?php if ($linked_logo_img): ?>
-      <div class="logo-img">
+      <div class="grid-6 logo-img">
         <?php print $linked_logo_img; ?>
       </div>
       <?php endif; ?>
-      <?php if ($site_name || $site_slogan): ?>
-      <?php $class = $site_name_hidden && $site_slogan_hidden ? ' element-invisible' : ''; ?>
-      <hgroup class="site-name-slogan<?php print $class; ?>">        
-        <?php if ($site_name): ?>
-        <?php $class = $site_name_hidden ? ' element-invisible' : ''; ?>
-        <?php if ($is_front): ?>        
-        <h1 class="site-name<?php print $class; ?>"><?php print $linked_site_name; ?></h1>
-        <?php else: ?>
-        <h2 class="site-name<?php print $class; ?>"><?php print $linked_site_name; ?></h2>
-        <?php endif; ?>
-        <?php endif; ?>
-        <?php if ($site_slogan): ?>
-        <?php $class = $site_slogan_hidden ? ' element-invisible' : ''; ?>
-        <h6 class="site-slogan<?php print $class; ?>"><?php print $site_slogan; ?></h6>
-        <?php endif; ?>
-      </hgroup>
+      <!-- Make this available. -->
+      <?php if ($main_menu): ?>
+        <nav class="navigation">
+          <div class="inline top-menu-links">
+            <?php print theme('links__system_main_menu', array('prefix' => '<div>', 'links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix', 'main-menu')), 'heading' => array('text' => t('Main menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
+          </div>
+        </nav>
       <?php endif; ?>
     </div>
     <?php endif; ?>
