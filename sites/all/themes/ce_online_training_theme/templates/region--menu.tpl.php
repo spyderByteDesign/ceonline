97c9afee7b9fde69108c39308da936065c3efcc0
Chris Zietlow <zietlow@palantir.net>
2014-02-23 23:33:57 -0500

    Added image block module

--- sites/all/themes/ce_online_training_theme/templates/region--menu.tpl.php
+++ sites/all/themes/ce_online_training_theme/templates/region--menu.tpl.php
@@ -1,12 +1,6 @@
 <div<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
-    <?php if ($secondary_menu): ?>
-    <nav class="navigation">
-      <div class="second-menu inline">
-        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix', 'secondary-menu')), 'heading' => array('text' => t('Secondary menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
-      </div>
-    </nav>
-    <?php endif; ?>
+
     <?php print $content; ?>
   </div>
 </div>
